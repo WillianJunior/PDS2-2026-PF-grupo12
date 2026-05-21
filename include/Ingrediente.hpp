f48aca3b11fbda7dadf420a9cf8311fe63f16e1f
root <root@givanildo.localdomain>
2026-05-21 19:14:26 -0300

    Fix issue

--- include/Ingrediente.hpp
+++ include/Ingrediente.hpp
@@ -19,14 +19,14 @@ private:
 public:
     Ingrediente(std::string nome, int quant, std::string unidade, std::string tipo); //construtor para ser usado em funções do tipo "adicionar ingrediente".
 
-    bool verificar_unidades(int quant, std::string unidade){}; //verifica se o item está disponivel na quantidade/unidade informada (compara com os valores locais).
+    bool verificar_unidades(int quant, std::string unidade); //verifica se o item está disponivel na quantidade/unidade informada (compara com os valores locais).
     bool validar_ingrediente(){}; //verifica se o ingrediente 
 
     //getters
-    std::string _getnomeI(){};
-    std::string _getunidadeI(){};
-    std::string _getTipoI(){};
-    int _getquantI(){};
+    std::string _getnomeI();
+    std::string _getunidadeI();
+    std::string _getTipoI();
+    int _getquantI();
 
     ~Ingrediente();
 };
